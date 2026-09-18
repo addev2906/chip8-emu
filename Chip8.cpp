1253ab9c34926b6b5e4d3f9fbe6d587395c99da2
Arjun Dubey <arjundubey2906@gmail.com>
2026-09-18 19:50:05 +0530

    add chip8 fetch,decode,execute cycle

--- Chip8.cpp
+++ Chip8.cpp
@@ -461,3 +461,17 @@ void Chip8::OP_Fx65()
 }
 
 void Chip8::OP_NULL(){}
+
+void Chip8::Cycle(){
+    opcode = (memory[pc]<<8u | memory[pc+1]);
+    pc+=2;
+
+    ((*this).*(table[(opcode & 0xF000)>>12u]))();
+
+    if(delayTimer>0){
+        delayTimer--;
+    }
+    if(soundTimer>0){
+        soundTimer--;
+    }
+}
